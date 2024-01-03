e2beb0ba49b4655979850c88eb8f7ce95bfd7020
MaxwellPechmann <maxwell.pechmann@gmail.com>
2024-01-03 15:38:42 +0100

    created Home

---
+++ frontend/src/components/pages/Home.tsx
@@ -0,0 +1,9 @@
+import { Topnav } from "../topnav/Topnav.tsx";
+
+export function HomePage() {
+    return (
+        <div className="w-screen h-screen">
+            <Topnav/>
+        </div>
+    )
+}
