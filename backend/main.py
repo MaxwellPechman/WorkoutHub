b23e729983fc08a07a500b9de30260cda4dcf4e1
MaxwellPechmann <maxwell.pechmann@gmail.com>
2024-01-03 15:48:17 +0100

    updated main

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,25 @@
+import uvicorn
+
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from backend.app.config import load_urls, load_host, load_port
+
+app_name = "main:app"
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=load_urls(),
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 if "__main__" == __name__:
-    pass
+    config = uvicorn.Config(host=load_host("SERVER_HOST"),
+                            port=load_port("SERVER_PORT"),
+                            log_level="info",
+                            app=app_name)
+    server = uvicorn.Server(config)
+    server.run()
